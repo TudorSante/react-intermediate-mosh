b91662f92a20662e902baed67702664778ce07b4
TudorSante <tudorsantejudean.com>
2023-08-26 16:56:39 +0300

    Handling Errors

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -13,31 +13,22 @@ const TodoList = () => {
   const fetchTodos = () =>
     axios
       .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data); // we don't need to return the whole query obj, just the data from its body
+      .then((res) => res.data);
 
-  /* define a React Query hook, where the config obj has 2 props: the queryKey
-  which unique identifier used internally for caching. Any time we retrieve data
-  from the backend, this data will be stored in the cached and accessible via
-  this key. We set this key to an array of one or more values, the first value 
-  is typically a string -> the type of values we want to store using this hook.
-  2nd prop: queryFn = the fct used to fetch the data from the backend. This fcn
-  should return a promise that resolves to data or throws an error.
-  This replaces the use of state hooks and effect hooks that we used in the past
-  and provides a more concise and easier to follow impl.
-
-  Benefits of using this react query approach: Auto retries, auto fetching, 
-  caching (the first time we fetch data, it is stored in the cache and will be
-  fresh for a certain period of time => getting the data in the future from 
-  the cache instead of the backend greatly improves the perf of our app).
-    */
-  const { data: todos } = useQuery({
+  /* the query hook obj also has a prop called error resp for keeping track of
+  the errors encountered during data fetching. When calling the query hook we
+  should also specify the types of error that we might encounted when fetching
+  data (that are indeed specific in this case to our axios lib). Thus we use
+  Error - the main axios class for errors. Pay attention to the types of the
+  data and error properties of our query hook. We need them in order to be 
+  able to access their values using the TS types check feature. */
+  const { data: todos, error } = useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: fetchTodos,
   });
 
-  // if (error) return <p>{error}</p>;
+  if (error) return <p>{error.message}</p>;
 
-  // use optional chaining to treat also the case when todos is undefined/null
   return (
     <ul className="list-group">
       {todos?.map((todo) => (
