587c9f52031f95a6a9e6bc25d2c43ec039f80722
TudorSante <tudorsantejudean.com>
2023-08-26 18:16:12 +0300

    Exercise - Fetching Data

---
+++ src/react-query/hooks/usePosts.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+const usePosts = () => {
+  const queryPosts = () =>
+    axios
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.data);
+
+  return useQuery<Post[], Error>({
+    queryKey: ["posts"],
+    queryFn: queryPosts,
+    staleTime: 1 * 60 * 1000,
+  });
+};
+
+export default usePosts;
