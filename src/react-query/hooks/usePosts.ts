c06cc5d56bc2f9ffe494b23b7c6ae720ae6ebf70
TudorSante <tudorsantejudean.com>
2023-08-26 18:56:15 +0300

    Parameterized Queries

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -8,14 +8,28 @@ interface Post {
   userId: number;
 }
 
-const usePosts = () => {
+const usePosts = (userId: number | undefined) => {
+  /* To query for a specific userId posts, use the 2nd param of the
+  get fcn - the config param, and inside the params obj add the field
+  you are interested in querying. */
   const queryPosts = () =>
     axios
-      .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+        params: {
+          userId,
+        },
+      })
       .then((res) => res.data);
 
+  /* api end point for fetching user posts: /users/1/posts. thus we
+  follow a hierarchical key description -> the data we are querying
+  gets more specific the deeper it becomes. 
+  userId is a param to this query - everytime this value changes 
+  react query should fetch the posts for that user from the backend.
+  This is very similar to the dependency array of effect hook.
+  */
   return useQuery<Post[], Error>({
-    queryKey: ["posts"],
+    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
     queryFn: queryPosts,
     staleTime: 1 * 60 * 1000,
   });
