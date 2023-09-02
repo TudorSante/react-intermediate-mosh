cfcf9ed7442e3be3e1656e16f341810ddeeb393a
TudorSante <tudorsantejudean.com>
2023-09-02 20:03:56 +0300

    Consolidated State Logic with a Reducer

--- src/state-management/Counter.tsx
+++ src/state-management/Counter.tsx
@@ -1,19 +1,27 @@
-import { useState } from 'react';
+import { useReducer, useState } from "react";
+import counterReducer from "./reducers/counterReducer";
 
 const Counter = () => {
-  const [value, setValue] = useState(0);
+  // const [value, setValue] = useState(0);
+  /* we use the reducer hook from react. 1st arg = our reducer fcn, 2nd arg =
+  the initial state of the state. */
+  const [value, dispatch] = useReducer(counterReducer, 0);
 
+  /* by using a reducer we make sure that our component is only responsible
+  for returning markup, it does not have any state management logic. Thus,
+  we have better separation of concerns and potentially, we can reuse our
+  reducer in other components that work with our counter. */
   return (
     <div>
       Counter ({value})
       <button
-        onClick={() => setValue(value + 1)}
+        onClick={() => dispatch({ type: "INCREMENT" })}
         className="btn btn-primary mx-1"
       >
         Increment
       </button>
       <button
-        onClick={() => setValue(0)}
+        onClick={() => dispatch({ type: "RESET" })}
         className="btn btn-primary mx-1"
       >
         Reset
