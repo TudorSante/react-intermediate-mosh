6310509b2d518e09f0b921e88925b602b8d95880
TudorSante <tudorsantejudean.com>
2023-09-05 12:58:36 +0300

    Managing App State with Zustand

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,12 +1,14 @@
 import LoginStatus from "./auth/LoginStatus";
+import useCounterStore from "./counter/store";
 import { useTasks } from "./tasks/TaskList";
 
 const NavBar = () => {
   const { tasks } = useTasks();
+  const { counter } = useCounterStore();
 
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">{tasks.length}</span>
+      <span className="badge text-bg-secondary">{counter}</span>
       <LoginStatus />
     </nav>
   );
--- src/state-management/counter/Counter.tsx
+++ src/state-management/counter/Counter.tsx
@@ -1,24 +1,7 @@
-import { useReducer, useState } from "react";
-
-interface Action {
-  type: "INCREMENT" | "RESET"; // union of literal values
-}
-
-/* a reducer centralisez the state management of a component. The first
-param = current state, 2nd param = action, obj describing what the user
-is trying to do. Reducer takes in the current state and an action and 
-returns the new state. */
-const counterReducer = (state: number, action: Action): number => {
-  if (action.type === "INCREMENT") return state + 1;
-  if (action.type === "RESET") return 0;
-  return state;
-};
+import useCounterStore from "./store";
 
 const Counter = () => {
-  // const [value, setValue] = useState(0);
-  /* we use the reducer hook from react. 1st arg = our reducer fcn, 2nd arg =
-  the initial state of the state. */
-  const [value, dispatch] = useReducer(counterReducer, 0);
+  const { counter, increment, reset } = useCounterStore();
 
   /* by using a reducer we make sure that our component is only responsible
   for returning markup, it does not have any state management logic. Thus,
@@ -26,17 +9,11 @@ const Counter = () => {
   reducer in other components that work with our counter. */
   return (
     <div>
-      Counter ({value})
-      <button
-        onClick={() => dispatch({ type: "INCREMENT" })}
-        className="btn btn-primary mx-1"
-      >
+      Counter ({counter})
+      <button onClick={() => increment()} className="btn btn-primary mx-1">
         Increment
       </button>
-      <button
-        onClick={() => dispatch({ type: "RESET" })}
-        className="btn btn-primary mx-1"
-      >
+      <button onClick={() => reset()} className="btn btn-primary mx-1">
         Reset
       </button>
     </div>
