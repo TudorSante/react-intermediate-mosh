782478ffd0261c30eb6c9f5eec230b823507deaf
TudorSante <tudorsantejudean.com>
2023-09-05 13:44:43 +0300

    Preventing Unnecessary Renders with Selectors

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -3,8 +3,12 @@ import useCounterStore from "./counter/store";
 import { useTasks } from "./tasks/TaskList";
 
 const NavBar = () => {
-  const { tasks } = useTasks();
-  const { counter } = useCounterStore();
+  /* this comp is dependent only on this prop and will rerender
+  only if counter changes, and not the rest of the store props.
+  Note that here we have used a Selector. */
+  const counter = useCounterStore((s) => s.counter);
+
+  console.log("Render NavBar");
 
   return (
     <nav className="navbar d-flex justify-content-between">
