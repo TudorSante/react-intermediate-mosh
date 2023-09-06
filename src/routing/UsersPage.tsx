584688404cba8bcab5927004370225709a88b939
TudorSante <tudorsantejudean.com>
2023-09-06 15:28:55 +0300

    Layout for Private Routes

--- src/routing/UsersPage.tsx
+++ src/routing/UsersPage.tsx
@@ -1,17 +1,7 @@
 import UserList from "./UserList";
-import { Navigate, Outlet } from "react-router-dom";
-import useAuth from "./hooks/useAuth";
+import { Outlet } from "react-router-dom";
 
 const UsersPage = () => {
-  const { user } = useAuth();
-  /* if the user not auth, redirect him to the login page. To 
-  keep our comp pure, we cannot use the navigate hook, as it
-  changes the url in the browser (can use it in an effect hook,
-  but by using this approach the component will be rendered first
-  and only then the user redirected... does not make much sense).
-  So we need to use here a Navigate element. */
-  if (!user) return <Navigate to="/login" />;
-
   return (
     <div className="row">
       <div className="col">
