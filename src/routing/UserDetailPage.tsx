e655681d95574991843318174a6daf2cd67b8c28
TudorSante <tudorsantejudean.com>
2023-09-06 13:47:18 +0300

    Getting Data about the Current Route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,13 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.toString());
+  console.log(searchParams.get("name"));
+  const location = useLocation();
+  console.log(location);
+
   return <p>User</p>;
 };
 
